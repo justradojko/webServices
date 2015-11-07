c7ebcd1fa03df2310cd7e873dec6e4cfad78c82c
Radojko Maksimovic <radojkomaksimovic@gmail.com>
2015-11-07 16:04:10 +0100

    Simple WebService

--- 01.WebService_basic/src/com/radojko/fibonacci/Fibonacci.java
+++ 01.WebService_basic/src/com/radojko/fibonacci/Fibonacci.java
@@ -4,6 +4,10 @@ import javax.jws.WebService;
 
 @WebService
 public class Fibonacci {
+	public void constructor(){
+		
+	}
+	
 	public int finbonacci(int n){
 		int[] fibArray = new int[] {1, 1};
 		int tempSum = 1;
@@ -12,8 +16,12 @@ public class Fibonacci {
 			throw new IllegalArgumentException();
 		}
 		
-		if (n > 1) {
-			for (int i = 2; i <= n; i++) {
+		if (n == 0){
+			return 0;
+		}
+		
+		if (n > 2) {
+			for (int i = 3; i <= n; i++) {
 				tempSum = fibArray[0] + fibArray[1];
 				fibArray[0] = fibArray[1];
 				fibArray[1] = tempSum;
--- 01.WebService_basic/src/com/radojko/fibonacci/fibonacciTestCase.java
+++ 01.WebService_basic/src/com/radojko/fibonacci/fibonacciTestCase.java
@@ -9,26 +9,44 @@ public class fibonacciTestCase {
 	@Test
 	public void test0() {
 		Fibonacci fib = new Fibonacci();
-		Assert.assertEquals(1, fib.finbonacci(0));
+		Assert.assertEquals(0, fib.finbonacci(0));
 	}	
 	
 	@Test
 	public void test1() {
 		Fibonacci fib = new Fibonacci();
 		Assert.assertEquals(1, fib.finbonacci(1));
+	}
+	
+	@Test
+	public void test2() {
+		Fibonacci fib = new Fibonacci();
+		Assert.assertEquals(1, fib.finbonacci(2));
 	}	
 	
 	@Test
 	public void test3() {
 		Fibonacci fib = new Fibonacci();
-		Assert.assertEquals(3, fib.finbonacci(3));
+		Assert.assertEquals(2, fib.finbonacci(3));
 	}
 	
 	@Test
 	public void test9() {
 		Fibonacci fib = new Fibonacci();
-		Assert.assertEquals(55, fib.finbonacci(9));
+		Assert.assertEquals(34, fib.finbonacci(9));
+	}
+	
+	@Test
+	public void test20() {
+		Fibonacci fib = new Fibonacci();
+		Assert.assertEquals(6765, fib.finbonacci(20));
 	}	
+	
+	@Test
+	public void test46() {
+		Fibonacci fib = new Fibonacci();
+		Assert.assertEquals(1836311903, fib.finbonacci(46));
+	}			
 	
 	@Test(expected=IllegalArgumentException.class)
 	public void testNegative() {
